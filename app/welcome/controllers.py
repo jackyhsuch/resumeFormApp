28f5df07ebed0c63132bd77d675ddc105b2999d3
Jacky <jackyhsu0102@gmail.com>
2017-08-04 14:50:51 +0800

    Revert "for heroku debug purpose"
    This reverts commit d0e40f5ea1cae7a793f65702356e355a91653447.

--- app/welcome/controllers.py
+++ app/welcome/controllers.py
@@ -28,7 +28,6 @@ def parseApplicationRequest(request):
         resume_filename = str(int(datetime.datetime.utcnow().timestamp())) + '.pdf'
 
         if resume_file.filename.endswith('.pdf'):
-            print(os.path.join(app.config['UPLOAD_FOLDER'], resume_filename))
             resume_file.save(os.path.join(app.config['UPLOAD_FOLDER'], resume_filename).replace("/","\\"))
         else:
             im = Image.open(resume_file)
