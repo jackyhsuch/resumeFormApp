89b5c9c2391aee5bba4ca33d47abda8bac7f512d
Jacky <jackyhsu0102@gmail.com>
2017-08-04 14:37:46 +0800

    heroku debug

--- app/applicant/views.py
+++ app/applicant/views.py
@@ -22,16 +22,18 @@ def uploaded_file(filename):
     import pdb
     try:
         # pdb.set_trace()
-        result = send_file('uploads/'+filename, as_attachment=True)
+        result = send_file('../uploads/'+filename, as_attachment=True)
 
         # result = send_from_directory(directory='uploads', filename=filename, as_attachment=True)
 
-        pdb.set_trace()
+        # pdb.set_trace()
 
         print(result)
     except Exception as e:
         print(e)
         result = e
+
+    # pdb.set_trace()
 
 
     return result
